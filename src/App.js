9bb10f595e75643cd511c63951fe3d4fcb41962b
James Yeates <jimibue@gmail.com>
2020-03-27 11:13:52 -0600

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar'
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h1>Hello</h1>
     </div>
   );
